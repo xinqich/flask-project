56c30f5ebd54287552c77c9ca1357fe6633c7e03
xinqich <timurcallo@gmail.com>
2025-09-07 17:30:41 +0300

    add templates
    Add template rendering for '/' and '/about'

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,13 @@
+from flask import render_template
 from app import app
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 @app.route('/hello')
 def hello():
